2eb94ba0ff31da74531fde35c1b70403b7021ed2
Matias Arratibel <125042945+MatiasTGOL@users.noreply.github.com>
2023-12-26 21:20:17 -0300

    Added animations while scrolling, adjusted some css.

--- src/app/components/body/body.js
+++ src/app/components/body/body.js
@@ -1,40 +1,46 @@
+// 'use client';
 import React, { useState, useEffect } from 'react';
 import './body.css';
 
 export default function Home() {
-
-    // Importar hooks adicionales según sea necesario
-    // Importar componentes como Contact, PitchSlate, etc.
-
-
-    // Estado y lógica similar a `data` y `computed` en Vue
-    const [currentSection, setCurrentSection] = useState(/* valor inicial */);
-    // ...otros estados
-
-    // Efectos secundarios para manejar eventos de montaje y desmontaje
     useEffect(() => {
-        // Montaje: Añadir event listeners, etc.
+        console.log("useEffect");
 
+        // Creación del IntersectionObserver
+        const observer = new IntersectionObserver((entries) => {
+            console.log("observer");
+            entries.forEach((entry) => {
+                console.log("entry", entry)
+                if (entry.isIntersecting) {
+                    console.log("is intersecting")
+                    entry.target.classList.add('show-section');
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, {
+            root: null, // Usa el viewport como área de root
+            rootMargin: '0px', // No hay margen
+            threshold: 0.15 // 10% del elemento debe estar visible
+        });
+
+        // Obteniendo todos los elementos con la clase 'hidden'
+        const hiddenElements = document.querySelectorAll('.hidden-section-l, .hidden-section-r');
+        hiddenElements.forEach(element => observer.observe(element));
+
+        // Función de limpieza
         return () => {
-            // Desmontaje: Limpiar event listeners, etc.
+            console.log("cleanup");
+            hiddenElements.forEach(element => observer.unobserve(element));
         };
     }, []);
 
-    // Métodos similares a Vue
-    const goToSection = (/* parámetros */) => {
-        // Lógica para navegar a una sección
-    };
-
-    // ...otros métodos
-
-    // Renderizado
     return (
         <div id="body" className="flex flex-col py-10 lg:p-24 items-center">
             <section className='section flex flex-col lg:flex-row'>
                 <div className='font-extrabold px-5 m-0 text-5xl sm:text-7xl lg:pr-20'>
                     <h1 className='txt-color5'>Full<span className="txt-color4">-</span>Stack</h1>
-                    Developer<span className='txt-color4'>.</span>
-                    <div className='text-lg sm:text-2xl font-normal pt-5 sm:pt-10'>
+                    <span>Developer</span><span className='txt-color4'>.</span>
+                    <div className='text-lg sm:text-xl font-normal pt-5 sm:pt-10'>
                         <p>
                             Mi pasión radica en diseñar sistemas que sean intuitivos y elegantes
                             en su funcionalidad, donde cada componente tenga un propósito claro
@@ -45,11 +51,10 @@ export default function Home() {
                     </div>
                 </div>
                 <div className='flex justify-center pt-24 lg:p-0'>
-                    <img src='/pfp.jpeg' className='pfp'/>
+                    <img src='/pfp.jpeg' className='pfp' />
                 </div>
             </section>
-            <section className='section'>Hello world</section>
-            <section className='section'>Hello world</section>
         </div>
     );
 };
+
--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -4,10 +4,10 @@ import './header.css';
 export default function Header() {
     return (
         <section className="header-container">
-            <header className="header bg-color2 ">
+            <header className="header">
                 <nav className="text-center font-bold navigation">
-                    <img src="/menu.png" className="menu-icon" alt="Menu" />
-                    <ul className="navigation-menu p-3">
+                    <img src="/menu2.png" className="menu-icon" alt="Menu" />
+                    <ul className="navigation-menu p-3 <¡">
                         <a href="/"><li>Inicio</li></a>
                         <a href="/"><li>Acerca de</li></a>
                         <a href="/"><li>Contacto</li></a>
