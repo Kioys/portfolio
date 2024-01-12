17671f886df0272367012cbbffd01a5ce2da6b61
Matias Arratibel <125042945+MatiasTGOL@users.noreply.github.com>
2024-01-12 12:52:08 -0300

    Removed unnecesary empty lines

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
 
 'use client';
 import Body from "./components/body/body";
-// Home.js
 import Header from "./components/header/header";
 
-
 export default function Home() {
-
   return (
     <div className="font-sans">
       <Header />
