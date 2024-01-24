33829b5a6f005eb4bf703023cfd9261f9d26d2d7
Matias Arratibel <125042945+MatiasTGOL@users.noreply.github.com>
2024-01-24 11:46:18 -0300

    Test bugfix that causes FOUC

--- src/app/components/ThemeToggle/ThemeToggle.js
+++ src/app/components/ThemeToggle/ThemeToggle.js
@@ -1,27 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
 const ThemeToggle = () => {
-    const [darkMode, setDarkMode] = useState(false);
-    const [firstTime, setFirstTime] = useState(true);
+    const [darkMode, setDarkMode] = useState(() => {
+        const osPrefersDark = window?.matchMedia && window?.matchMedia('(prefers-color-scheme: dark)')?.matches;
+        const userPrefersDark = localStorage.getItem('theme') === 'dark';
+        return userPrefersDark || osPrefersDark;
+    });
 
     useEffect(() => {
-        if (firstTime) {
-            const preferDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-            setDarkMode(preferDarkMode);
-            setFirstTime(false);
-        }
-        else {
-            if (darkMode) {
-                document.documentElement.setAttribute('data-theme', 'dark');
-            } else {
-                document.documentElement.removeAttribute('data-theme');
-            }
-        }
-
+        document.documentElement.setAttribute('data-theme', darkMode ? 'dark' : 'light');
+        localStorage.setItem('theme', darkMode ? 'dark' : 'light');
     }, [darkMode]);
 
+    const toggleTheme = () => {
+        setDarkMode(prevMode => !prevMode);
+    };
+
     return (
-        <button onClick={() => setDarkMode(prevMode => !prevMode)}>
+        <button onClick={toggleTheme}>
             {darkMode ? <img src="/icons/moon.png" alt="moon" className='h-8 w-8' /> : <img src="/icons/cloudy.png" alt="sun" className='h-8 w-8' />}
         </button>
     );
